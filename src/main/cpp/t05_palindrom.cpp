7f9617e4eea508c29b19a0552ac44ae8c986bb19
Polina <55378557+chipa371@users.noreply.github.com>
2019-12-12 02:03:54 +0300

    Update t05_palindrom.cpp

--- src/main/cpp/t05_palindrom.cpp
+++ src/main/cpp/t05_palindrom.cpp
@@ -23,5 +23,20 @@
 using namespace std;
 
 int t05_palindrom() {
-
+    string s;
+    cin >> s;
+    int n = s.length();
+    bool a = 1;
+    
+    for(int i = 0; i < n/2; i++){
+        if(s[i] == s[n-1-i])
+            a = a&&1;
+        else
+            a = a&&0;
+    }
+    if(a == 0)
+        cout << "no";
+    else
+        cout << "yes";
+    
 }
